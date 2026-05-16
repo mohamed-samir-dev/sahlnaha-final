bb8fa4b15469bd274bfb39f8acd09930e82c5ca2
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-05-16 22:40:05 +0300

    Add

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,15 @@ import BrowseCategories from "./components/BrowseCategories";
 import PromoBanners from "./components/PromoBanners";
 import ProductGrid from "./components/products/ProductGrid";
 import CustomerReviews from "./components/CustomerReviews";
-import AnimatedBackground from "./components/AnimatedBackground";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen">
-      <AnimatedBackground />
-      <div className="relative">
-        <HeroSection />
-        <BrowseCategories />
-        <ProductGrid />
-        <PromoBanners />
-        <CustomerReviews />
-      </div>
+    <main className="min-h-screen">
+      <HeroSection />
+      <BrowseCategories />
+      <ProductGrid />
+      <PromoBanners />
+      <CustomerReviews />
     </main>
   );
 }
